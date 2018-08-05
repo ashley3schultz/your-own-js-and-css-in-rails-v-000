0f7f65f85b29e79454bcdc90c33642786db8cab6
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-05 23:34:10 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,8 +1,9 @@
 function hideWhenClicked(){
-  var x = document.getElementById("hide_this");
+  const x = $('#hide_this')
   if (x.style.display === "none") {
       x.style.display = "block";
   } else {
       x.style.display = "none";
   }
+}
 }
