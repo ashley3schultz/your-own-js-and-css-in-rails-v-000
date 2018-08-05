e07f2403a35dfc6ad1d28e6a20f6fb577ea2a868
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-05 23:34:32 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,9 +1,4 @@
 function hideWhenClicked(){
   const x = $('#hide_this')
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
-}
+  x.style.display = "none"
 }
